3bf31c80570108eb6a338de9952b642608ff58d6
chinhonnang <87120671+chinhonnang@users.noreply.github.com>
2021-07-25 14:05:32 +0800

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+const scrapuloGenerator extend(PlanetGenerator, 
+{
+    getColor(position){
+        var block = this.getBlock(position);
+
+        Tmp.c1.set(block.mapColor).a = 1 - block.albedo;
+        return Tmp.c1;
+},
+
+const scrapulo = new JavaAdapter(Planet, {}, "scrapulo", Planets.sun, 4, 0.9);
+scrapulo.generator = new SerpuloPlanetGenerator();
+scrapulo.mesh = new HexMesh(scrapulo, 5);
+scrapulo.orbitRadius = 100;
+scrapulo.orbitTime = 1500;
+scrapulo.rotateTime = 130;
+scrapulo.bloom = true;
+scrapulo.accessible = true;
+scrapulo.startSector = 51;
+scrapulo.hasAtmosphere = false;
+scrapulo.atmosphereColor = Color.valueOf("63A53cFF");
+scrapulo.atmosphereRadIn = 0.0;
+scrapulo.atmosphereRadOut = 0.0;
+scrapulo.alwaysUnlocked = true;
+scrapulo.localizedName = "scrapulo";
