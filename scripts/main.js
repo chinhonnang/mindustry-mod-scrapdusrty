f989e3ecdb9bc3a5daa8619cc3eedcaccd993bc3
chinhonnang <87120671+chinhonnang@users.noreply.github.com>
2021-07-25 16:43:11 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-const scrapulo = new JavaAdapter(Planet, {}, "scrapulo", Planets.sun, 2, 1);
+const scrapulo = new JavaAdapter(Planet, {}, "scrapulo", Planets.sun, 2.8, 1);
 scrapulo.generator = new SerpuloPlanetGenerator;
 scrapulo.localizedName = "scrapulo";
 
 
 
-const scrapulo_inlet = new SectorPreset("scarpulo_inlet", scrapulo, 51);
+const scrapulo_inlet = new SectorPreset("scarpulo_inlet", scrapulo, 13);
 scrapulo_inlet.difficulty = 2; 
 scrapulo_inlet.alwaysUnlocked = true;
 scrapulo_inlet.captureWave = 30; 
